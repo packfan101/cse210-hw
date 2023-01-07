5655e2850954aaeb2bf5d699ae8e9e8da766a8dd
packfan101 <jen20082@byui.edu>
2023-01-07 11:37:46 -0700

    Completed Code for Prep2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Declare Variables
+        string letter;
+        string sign;
+        
+        //Get input from user and convert to a float
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+
+        //Calculate letter grade
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //Calculate sign
+        if ((percent % 10) >= 7)
+        {
+            sign = "+";
+        }
+        else if ((percent % 10) < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        //Eliminate A+, F+, and F-
+        if (percent < 60 || percent > 93)
+        {
+            sign = "";
+        }
+
+        //Output
+        Console.WriteLine($"Grade: {letter}{sign}");
+
+        //Provide Pass/Fail message
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you needed at least a C- to pass the class. Don't give up, you will do better next time.");
+        }
     }
 }
