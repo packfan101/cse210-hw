a41b42c91074887f1122543cfc42fa141fceafbf
packfan101 <jen20082@byui.edu>
2023-03-02 17:48:02 -0700

    Complete develop 05 assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square s1 = new Square("green", 6);
+        shapes.Add(s1);
+
+        Circle c1 = new Circle("red", 3);
+        shapes.Add(c1);
+
+        Rectangle r1 = new Rectangle("purple", 6, 4);
+        shapes.Add(r1);
+
+        foreach (Shape shape in shapes){
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}\n");
     }
+    }
+
+    
+    
 }
